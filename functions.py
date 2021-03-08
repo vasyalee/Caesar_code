7a59d9c2d5850cc16898b0d5589f63501f66f33f
vasyalee <flightsho@gmail.com>
2021-03-08 16:02:43 +0300

    Added advanced decryption feature for russian language.

--- functions.py
+++ functions.py
@@ -1,18 +1,18 @@
 def rus_encrypt(ru_str):
-    k = int(input('Enter shift'))
+    k = int(input('Enter shift\n'))
     res = ''
     for letter in ru_str:
         if letter.islower() and letter in rus_lower:
-            res += rus_lower[rus_lower.find(letter) + k]
+            res += rus_lower[rus_lower.find(letter) + int(k)]
         elif letter.isupper() and letter in rus_upper:
-            res += rus_upper[rus_upper.find(letter) + 35]
+            res += rus_upper[rus_upper.find(letter) + int(k)]
         else:
             res += letter
     return res
 
 
 def eng_encrypt(eng_str):
-    k = int(input('Enter shift'))
+    k = int(input('Enter shift\n'))
     res = ''
     for letter in eng_str:
         if letter.islower() and letter in eng_lower:
@@ -24,19 +24,34 @@ def eng_encrypt(eng_str):
     return res
 
 def rus_decrypt(ru_encrypted):
-    k = int(input('Enter shift'))
-    res = ''
-    for letter in ru_encrypted:
-        if letter.islower() and letter in rus_lower:
-            res += rus_lower[(rus_lower.find(letter) - k) % 32]
-        elif letter.isupper() and letter in rus_upper:
-            res += rus_upper[(rus_upper.find(letter) - k) % 32]
-        else:
-            res += letter
-    return res
+    k = input('Enter shift or type "all" to try them all\n')
+    if k == 'all':
+        sub_list = []
+        res_for_all_shifts = []
+        for k in range(0, 33):
+            for letter in ru_encrypted:
+                if letter.islower() and letter in rus_lower:
+                    sub_list.append(rus_lower[(rus_lower.find(letter) - int(k)) % 32])
+                elif letter.isupper() and letter in rus_upper:
+                    sub_list.append(rus_upper[(rus_upper.find(letter) - int(k)) % 32])
+                else:
+                    sub_list.append(letter)
+            res_for_all_shifts.append(''.join(sub_list))
+            sub_list.clear()
+        return '\n'.join(res_for_all_shifts)
+    else:
+        res = ''
+        for letter in ru_encrypted:
+            if letter.islower() and letter in rus_lower:
+                res += rus_lower[(rus_lower.find(letter) - int(k)) % 32]
+            elif letter.isupper() and letter in rus_upper:
+                res += rus_upper[(rus_upper.find(letter) - int(k)) % 32]
+            else:
+                res += letter
+        return res
 
 def eng_decrypt(eng_encrypted):
-    k = input('Enter shift or type "all" to try them all')
+    k = input('Enter shift or type "all" to try them all\n')
     if k == 'all':
         sub_list = []
         res_for_all_shifts = []
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from functions import *
 
-str = input('Enter string\n')
-task = input('Encrypt or decrypt? (e d)\n')
-lang = input('Language? (rus eng)\n')
+while True:
+
+    str = input('Enter string\n')
+    task = input('Encrypt or decrypt? (e d)\n')
+    lang = input('Language? (rus eng)\n')
 
 
-if lang == 'eng':
-    if task == 'e':
-        print(eng_encrypt(str))
-    elif task == 'd':
-        print(eng_decrypt(str))
+    if lang == 'eng':
+        if task == 'e':
+            print(eng_encrypt(str))
+        elif task == 'd':
+            print(eng_decrypt(str))
 
-elif lang == 'rus':
-    if task == 'e':
-        print(rus_encrypt(str))
-    elif task == 'd':
-        print(rus_decrypt(str))
+    elif lang == 'rus':
+        if task == 'e':
+            print(rus_encrypt(str))
+        elif task == 'd':
+            print(rus_decrypt(str))
